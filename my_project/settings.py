810708bb93dd3a063b5e0a10dd280c3d53833b5b
Ayah K Alrifai <alrefayayah@gmail.com>
2019-06-14 02:41:23 -0700

    Added django-heroky

--- my_project/settings.py
+++ my_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import os.path
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,3 +142,5 @@ EMAIL_HOST_USER = "alrefayayah@gmail.com"
 EMAIL_HOST_PASSWORD = "HaYa.IaFiRlA.97"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
